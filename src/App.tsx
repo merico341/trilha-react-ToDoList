2021b7be3b28bb8e9053bd81627e105ef6dc36e9
merico341 <lonetfricoto@gmail.com>
2025-10-04 14:46:05 -0300

    creating: React component 'buttonIcon'

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Text from "./components/text";
 import Icon from "./components/icon"
 import Badge from "./components/badge";
 import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 import CheckIcon from "./assets/icons/Check.svg?react";
 import PensilIcon from "./assets/icons/Pencil.svg?react";
 import PlusIcon from "./assets/icons/Plus.svg?react";
@@ -41,6 +42,12 @@ export default function App() {
       </div>
 
       <div><Button icon={PlusIcon}> nova tarefa</Button></div>
+
+      <div>
+        <ButtonIcon icon={TrashIcon}/>
+        <ButtonIcon icon={TrashIcon} variant={"secondary"}/>
+        <ButtonIcon icon={TrashIcon} variant={"tertiary"}/>
+      </div>
     </div>
   )
 }

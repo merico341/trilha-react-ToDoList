a55411ed77bf5c2cf9eca9cc57e88d20da01fc11
merico341 <lonetfricoto@gmail.com>
2025-10-04 17:23:37 -0300

    creating: React component 'Skeleton'

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import Text from "./components/text";
 import Icon from "./components/icon"
 import Badge from "./components/badge";
 import Button from "./components/button";
-import InputText from "./components/input-text";
 import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
+import InputCheckBox from "./components/input-checkbox";
 import Card from "./components/card";
 import Container from "./components/container";
-import InputCheckBox from "./components/input-checkbox";
+import Skeleton from "./components/skeleton";
 import CheckIcon from "./assets/icons/Check.svg?react";
 import PensilIcon from "./assets/icons/Pencil.svg?react";
 import PlusIcon from "./assets/icons/Plus.svg?react";
@@ -64,6 +65,12 @@ export default function App() {
 
         <div>
           <Card size="md">Olá mundo</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6"/>
+          <Skeleton className="h-6"/>
+          <Skeleton className="w-96 h-6"/>
         </div>
       </div>
     </Container>

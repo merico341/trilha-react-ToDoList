0b4e78a11c538e6cae13daf5362cda93c3798527
merico341 <lonetfricoto@gmail.com>
2025-10-04 10:55:56 -0300

    creating: React component 'bagde'

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Text from "./components/text";
 import Icon from "./components/icon"
+import Badge from "./components/badge";
 import CheckIcon from "./assets/icons/Check.svg?react";
 import PensilIcon from "./assets/icons/Pencil.svg?react";
 import PlusIcon from "./assets/icons/Plus.svg?react";
@@ -31,6 +32,11 @@ export default function App() {
         <Icon svg={SpinnerIcon} animate/>
         <Icon svg={PensilIcon}/>
         <Icon svg={XIcon}/>
+      </div>
+
+      <div>
+        <Badge variant={'secondary'}>5</Badge>
+        <Badge variant={'primary'}>2 de 5</Badge>
       </div>
     </div>
   )

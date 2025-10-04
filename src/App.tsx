399520da999e65184e9b727d570371dce0f09a01
merico341 <lonetfricoto@gmail.com>
2025-10-04 11:46:28 -0300

    creating: React component 'Button'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Text from "./components/text";
 import Icon from "./components/icon"
 import Badge from "./components/badge";
+import Button from "./components/button";
 import CheckIcon from "./assets/icons/Check.svg?react";
 import PensilIcon from "./assets/icons/Pencil.svg?react";
 import PlusIcon from "./assets/icons/Plus.svg?react";
@@ -38,6 +39,8 @@ export default function App() {
         <Badge variant={'secondary'}>5</Badge>
         <Badge variant={'primary'}>2 de 5</Badge>
       </div>
+
+      <div><Button icon={PlusIcon}> nova tarefa</Button></div>
     </div>
   )
 }

e25278ca2edec3217ad14740e35a9ab72b6d9bf3
merico341 <lonetfricoto@gmail.com>
2025-10-05 14:42:54 -0300

    creating: React Hook 'useTask'

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -2,16 +2,22 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/Plus.svg?react";
 import TaksItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
 
 export default function TasksList() {
     const { tasks } = useTasks();
+    const { prepareTask } = useTask();
 
     console.log(tasks);
+
+    function handleNewTask() {
+        prepareTask()
+    }
 
     return (
         <>
             <section>
-                <Button icon={PlusIcon} className="w-full">Nova tarefa</Button>
+                <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>Nova tarefa</Button>
             </section>
 
             <section className="space-y-2">

8e603efde7b512d5f62a22e7a22570cc06f5d1f0
merico341 <lonetfricoto@gmail.com>
2025-10-06 16:19:10 -0300

    moddifying: React core component 'TasksSummary' and React hook 'use-task' to update the badge in aplication

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,17 +1,20 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 
 export default function TasksSummary() {
+    const { createTasksCount, concludedTaskCount } = useTasks();
+    
     return (
         <>
             <div className="flex items-center gap-2">
                 <Text variant={"body-sm-bold"} className="!text-gray-300">Tarefas criadas</Text>
-                <Badge variant={"secondary"}>5</Badge>
+                <Badge variant={"secondary"}>{createTasksCount}</Badge>
             </div>
             <div className="flex items-center gap-2">
                 <Text variant={"body-sm-bold"} className="!text-gray-300">Concluídas</Text>
-                <Badge variant={"primary"}>2 de 5</Badge>
+                <Badge variant={"primary"}>{concludedTaskCount} de {createTasksCount}</Badge>
             </div>
         </>
     )
